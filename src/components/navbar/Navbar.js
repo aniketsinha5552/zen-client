ea824605877e5ef3b45565d0754fd1f4b415583d
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-12-23 17:58:26 +0530

    feat: [navbar] logout confirm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import { Icon } from "@iconify/react";
 import { useDispatch, useSelector } from "react-redux";
-import { IconButton } from "@mui/material";
+import { Dialog, IconButton } from "@mui/material";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { buttonClick } from "../../assets/functions/clickSound";
 import { updateUser } from "../../redux/reducers/userSlice";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 import LofiPlayer from "../lofiPlayer/LofiPlayer";
+import { newShade } from "../../utils/newShade";
+import { toastify } from "../../utils/toastify";
 
 const Navbar = () => {
   const user = useSelector((state) => state.user.user);
@@ -16,11 +18,14 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [open,setOpen]= useState(false)
+
   const logout = () => {
     buttonClick.play();
     dispatch(updateUser(null));
     auth.signOut();
     navigate("/");
+    toastify("success","Logged Out!")
   };
 
   return (
@@ -38,12 +43,23 @@ const Navbar = () => {
           <IconButton
             title="logout"
             style={{ backgroundColor: { theme } }}
-            onClick={logout}
+            onClick={()=>setOpen(true)}
           >
             <ExitToAppIcon />
           </IconButton>
         </span>
       </div>
+      <Dialog open={open} onClose={()=>setOpen(false)}>
+        <div className={styles.logoutConfirm} style={{backgroundColor: newShade(theme,10)}}>
+        <h3 className={styles.confirmText}>
+          So long! {user.username}
+        </h3>
+        <div className={styles.btnContainer}>
+        <button className={styles.stayBtn}onClick={()=>setOpen(false)}>Stay</button>
+        <button className={styles.leaveBtn} onClick={logout}>Leave</button>
+        </div>
+        </div>
+      </Dialog>
     </div>
   );
 };
